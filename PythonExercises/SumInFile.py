6e27718faeb6e600225c455d16d3c2a12055289a
javedshaik <jshaik@terpmail.umd.edu>
2017-01-12 13:13:03 -0500

    Used regular expression to find the sum of all integers in the data file.

---
+++ PythonExercises/SumInFile.py
@@ -0,0 +1,11 @@
+import re
+file = open('regex_sum_342237')
+num = list()
+for line in file:
+	sum = re.findall('[0-9]+', line)
+	num = num + sum
+total = 0
+for n in num:
+	total = total + int(n)
+print total
+	
